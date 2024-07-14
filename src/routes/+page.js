861b319f1a651d9d64470227ef3c67ca3f569775
Michael Snook <michael.snook@gmail.com>
2024-07-14 18:33:11 +0530

    Add index page fetching from DB

---
+++ src/routes/+page.js
@@ -0,0 +1,10 @@
+import { supabase } from '$lib'
+import { error } from '@sveltejs/kit'
+
+export async function load() {
+	let { data, error: fetchError } = await supabase.from('campaigns').select('*')
+	console.log(`this is the data:`, data)
+	if (fetchError) error(500, fetchError.message)
+	if (data?.length === 0) error(404, 'Not found')
+	return { campaigns: data }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	/** @type {import('./$types').PageData} */
+  const { data } = $props()
+  console.log(`inside the component, this is the data`, data)
+</script>
+
+<h1>Welcome to this little whip tracker</h1>
+<ul>
+  {#each data.campaigns as campaign}
+    <li>
+      <a href="/{campaign.slug}">
+        <p><strong>{campaign.title}</strong></p>
+        <p>{campaign.description}</p>
+      </a>
+    </li>
+  {/each}
+</ul>
